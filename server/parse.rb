f19808e66c07e524715df1a530cd9015fba889fa
Dmitry Petruk <bavaria95@gmail.com>
2015-08-22 23:15:50 +0300

    also parsing EUR

--- server/parse.rb
+++ server/parse.rb
@@ -1,7 +1,17 @@
 require 'open-uri'
 require 'nokogiri'
 
-usd = Nokogiri::HTML(open("http://minfin.com.ua/currency/auction/usd/buy/all/?sort=time&order=desc")).
-	css("div[class='au-mid-buysell']").to_a[0..1].map{|t| t.text.strip.split(/\n/).map{|x| x.strip}}
+def get_currency url
+	Nokogiri::HTML(open(url)).
+				css("div[class='au-mid-buysell']").to_a[0..1].map{|t| t.text.strip.split(/\n/).map{|x| x.strip}}
+end
+
+url_usd = "http://minfin.com.ua/currency/auction/usd/buy/all/?sort=time&order=desc"
+url_eur = "http://minfin.com.ua/currency/auction/eur/buy/all/?sort=time&order=desc"
+
+usd = get_currency url_usd
+eur = get_currency url_eur
+
 
 p usd
+p eur
