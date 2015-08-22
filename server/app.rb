02995d6e0e77593be0a51ee12ee8bb1d34dbee92
Dmitry Petruk <bavaria95@gmail.com>
2015-08-22 23:34:51 +0300

    removed name of domestic currency

--- server/app.rb
+++ server/app.rb
@@ -1,13 +1,26 @@
 require 'sinatra'
+require 'open-uri'
+require 'nokogiri'
+require 'json'
+
+def get_currency url
+	doc = Nokogiri::HTML(open(url)).
+		css("div[class='au-mid-buysell']").to_a[0..1].map{|t| t.text.strip.split(/\n/).
+			map{|x| x.strip}}.map{|r| [r[0], r[1].gsub(/[^\d,]/, '').gsub(',', '.')]}
+end
+
+url_usd = "http://minfin.com.ua/currency/auction/usd/buy/all/?sort=time&order=desc"
+url_eur = "http://minfin.com.ua/currency/auction/eur/buy/all/?sort=time&order=desc"
 
 get '/' do
 	"Hello world"
 end
 
-get '/usd' do 
-	"42"
+get '/usd' do
+	content_type :json 
+	get_currency(url_usd).to_json
 end
 
 get '/eur' do 
-	"73"
+	get_currency(url_eur).to_s
 end
