ee6f9b5a381a564e1383d7dd11a117f414a34d0e
Dmitry Petruk <bavaria95@gmail.com>
2015-08-24 23:13:27 +0300

    putting values of currencies into table cells

--- client/javascript/XHRExample.js
+++ client/javascript/XHRExample.js
@@ -36,6 +36,32 @@ XHRExample.recieveData = function () {
         
         alert(this.XHRObj.responseText);
         
+        var usd = curr['usd'];
+        
+        var usd_buy_label = usd[0][0];
+        document.getElementById("usd_buy_label").innerHTML =  usd_buy_label;
+        var usd_buy_number = usd[0][1];
+        document.getElementById("usd_buy_number").innerHTML =  usd_buy_number;
+        
+        var usd_sell_label = usd[1][0];
+        document.getElementById("usd_sell_label").innerHTML =  usd_sell_label;
+        var usd_sell_number = usd[1][1];
+        document.getElementById("usd_sell_number").innerHTML =  usd_sell_number;
+        
+        
+        var eur = curr['eur'];
+        
+        var eur_buy_label = eur[0][0];
+        document.getElementById("eur_buy_label").innerHTML =  eur_buy_label;
+        var eur_buy_number = eur[0][1];
+        document.getElementById("eur_buy_number").innerHTML =  eur_buy_number;
+        
+        var eur_sell_label = eur[1][0];
+        document.getElementById("eur_sell_label").innerHTML =  eur_sell_label;
+        var eur_sell_number = eur[1][1];
+        document.getElementById("eur_sell_number").innerHTML =  eur_sell_number;
+        
+        document.getElementById("tbl").style.visibility = "visible";
         
 } 
         
